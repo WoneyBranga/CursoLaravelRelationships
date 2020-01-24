c885ea9f7a14f85823eb2844c826fabf7488d8a3
woneybranga <woney.branga@gmail.com>
2020-01-24 10:28:56 -0300

    aula 06 - criando tabelas para estudos oneToMany

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,17 @@
 <?php
 
+/**
+ * One To One
+ */
 Route::get('one-to-one', 'OneToOneController@oneToOne');
 Route::get('one-to-one-inverse', 'OneToOneController@oneToOneInverse');
 Route::get('one-to-one-insert', 'OneToOneController@oneToOneInsert');
+
+/**
+ * One To Many
+ */
+Route::get('one-to-many', 'OneToMany@oneToMany');
+
 
 Route::get('/', function () {
     return view('welcome');
