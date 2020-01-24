5396856fe7a2937474c66e338e85b8703503e913
woneybranga <woney.branga@gmail.com>
2020-01-24 18:12:13 -0300

    Aula 16 many to many insert

--- app/Http/Controllers/ManyToManyController.php
+++ app/Http/Controllers/ManyToManyController.php
@@ -29,7 +29,30 @@ class ManyToManyController extends Controller
         foreach ($cities as $city) {
             echo ' - Cidades: ' . $city->name . PHP_EOL;
         }
-
-
     }
+
+    public function manyToManyInsert()
+    {
+        $dataForm = [ 1, 2, 3 ];
+
+        $company = Company::find(2);
+        echo 'Empresa: ' . $company->name . PHP_EOL;
+
+        #$company->cities()->attach($dataForm);
+        # problema do attach é que podemos ter valor repetido...
+
+        $company->cities()->sync($dataForm);
+        # o sync sincroniza dados com informados, se tinhamos opções
+        # alem das aqui editadas, estas sumirao.. vale o q sincronizar..
+
+        $company->cities()->detach(1);
+        # remove um item/array de itens
+
+
+        $cities = $company->cities;
+        foreach ($cities as $city) {
+            echo ' - Cidades: ' . $city->name . PHP_EOL;
+        }
+    }
+
 }
